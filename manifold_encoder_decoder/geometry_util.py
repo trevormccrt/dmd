2c2271eb2532f948e12674a92399abb8289fac91
trevormccrt <trevormccrt@gmail.com>
2023-01-10 18:44:47 -0500

    remove deprecated functions

--- manifold_encoder_decoder/geometry_util.py
+++ manifold_encoder_decoder/geometry_util.py
@@ -2,24 +2,9 @@ import numpy as np
 import torch
 
 
-def integrated_angle_metric(angles):
-    angles_rolled = np.roll(angles, 1, axis=-1)
-    return np.abs(angles - angles_rolled)
-
-
-def torch_integrated_angle_metric(angles):
-    angles_rolled = torch.roll(angles, 1, dims=-1)
-    return torch.abs(angles - angles_rolled)
-
-
 def integrated_point_metric(points):
     rolled_points = torch.roll(points, 1, dims=-2)
     return torch.sum(torch.sqrt(torch.sum(torch.square(points[..., 1:, :] - rolled_points[..., 1:, :]), dim=-1) + 1e-13), dim=-1)
-
-
-def densely_sample_angles(angles, n_samples):
-    angles_rolled = np.roll(angles, 1, axis=1)
-    return np.linspace(start=angles, stop=angles_rolled, num=n_samples, axis=-1)
 
 
 def angles_to_ring(angles):
@@ -41,42 +26,6 @@ def torch_linspace(start, stop, num: int):
         steps = steps.unsqueeze(-1)
     out = start[None] + steps * (stop - start)[None]
     return out
-
-
-def resample_points_ring(points, n_samples):
-    angles = torch.atan2(points[:, :, 1], points[:, :, 0])
-    sampled_angles = torch.transpose(torch.transpose(torch_linspace(angles, torch.roll(angles, 1, dims=-1), n_samples), 0, 2), 0, 1)
-    points = torch_angles_to_ring(sampled_angles)
-    return angles, points
-
-
-def resample_points_ring_directed(points, n_samples, direction):
-    angles = torch.atan2(points[:, :, 1], points[:, :, 0])
-    rand_mask = direction * 2 * np.pi
-    angles_remap = torch.remainder(angles, 2 * np.pi) - rand_mask.to(angles.device)
-    rolled_angles = torch.roll(angles_remap, 1, dims=-1)
-    angle_distances = torch.abs(angles_remap - rolled_angles)
-    sampled_angles = torch.transpose(torch.transpose(torch_linspace(angles_remap, rolled_angles, n_samples), 0, 2), 0, 1)
-    new_points = torch_angles_to_ring(sampled_angles)
-    return angles, new_points, angle_distances
-
-
-def walk_manifold(start_phases, end_phases, n_points):
-    manifold_dim = start_phases.shape[-1]
-    all_start = torch.tile(torch.unsqueeze(start_phases, dim=-2), (n_points, 1))
-    all_end = torch.tile(torch.unsqueeze(end_phases, dim=-2), (n_points, 1))
-    walked_phases = torch_linspace(start_phases, end_phases, n_points)
-    perm = list(np.roll(list(range(len(walked_phases.shape))), -1))
-    walked_phases = torch.permute(walked_phases, perm)
-    walked_phases = torch.transpose(walked_phases, -1, -2)
-    outputs = None
-    for i in range(manifold_dim):
-        this_ouput = torch.cat([all_end[...,:, :i], torch.unsqueeze(walked_phases[..., :, i], -1) , all_start[..., :, i+1:]], dim=-1)
-        if outputs is None:
-            outputs = this_ouput
-        else:
-            outputs = torch.cat([outputs, this_ouput[..., 1:, :]], dim=-2)
-    return outputs
 
 
 def periodic_reflections(point_list):
@@ -114,19 +63,3 @@ def closest_points_periodic(point_list):
                                                              [list_b.size(-1)])), -2)
     matched_points_b = torch.squeeze(torch.gather(remapped_b_unflat, -2, torch.tile(torch.unsqueeze(torch.unsqueeze(matches,-1), -1), [list_b.size(-1)])), -2)
     return min_distances, matched_points_a, matched_points_b, matches
-
-
-class CircleDistance(torch.nn.Module):
-    def __init__(self, init_shift=torch.from_numpy(np.array([0.0, 0.0])), init_rad=torch.from_numpy(np.array([1.0]))):
-        super().__init__()
-        self.shift = torch.nn.Parameter(init_shift)
-        self.rad = torch.nn.Parameter(init_rad)
-
-    def forward(self, inputs):
-        return torch.square(torch.sum(torch.square(inputs - self.shift), dim=-1) - torch.square(self.rad))
-
-    def circularize(self, inputs):
-        return (inputs - self.shift)/self.rad
-
-    def phases(self, inputs):
-        return torch.atan2(inputs[..., 1], inputs[..., 0])
--- manifold_encoder_decoder/geometry_util_test.py
+++ manifold_encoder_decoder/geometry_util_test.py
@@ -2,13 +2,31 @@ import numpy as np
 import torch
 
 from manifold_encoder_decoder import geometry_util
+from manifold_encoder_decoder.geometry_util import torch_linspace, torch_angles_to_ring
+
+
+def _integrated_angle_metric(angles):
+    angles_rolled = np.roll(angles, 1, axis=-1)
+    return np.abs(angles - angles_rolled)
+
+
+def _densely_sample_angles(angles, n_samples):
+    angles_rolled = np.roll(angles, 1, axis=1)
+    return np.linspace(start=angles, stop=angles_rolled, num=n_samples, axis=-1)
+
+
+def _resample_points_ring(points, n_samples):
+    angles = torch.atan2(points[:, :, 1], points[:, :, 0])
+    sampled_angles = torch.transpose(torch.transpose(torch_linspace(angles, torch.roll(angles, 1, dims=-1), n_samples), 0, 2), 0, 1)
+    points = torch_angles_to_ring(sampled_angles)
+    return angles, points
 
 
 def test_integrated_metric():
     n_resamples = 200
     angles = np.sort(np.random.uniform(-np.pi, np.pi, (1, 4)), axis=1)
-    angular_distance = geometry_util.integrated_angle_metric(angles)
-    resampled_angles = geometry_util.densely_sample_angles(angles, n_resamples)
+    angular_distance = _integrated_angle_metric(angles)
+    resampled_angles = _densely_sample_angles(angles, n_resamples)
     points = torch.from_numpy(geometry_util.angles_to_ring(resampled_angles))
     point_metric = geometry_util.integrated_point_metric(points)
     np.testing.assert_allclose(point_metric, angular_distance, rtol=1e-3)
@@ -18,58 +36,11 @@ def test_ring_interpolation():
     n_samples = 100
     actual_angles = torch.from_numpy(np.random.uniform(-np.pi, np.pi, (1, 10)))
     points = geometry_util.torch_angles_to_ring(actual_angles)
-    pred_angles, interpolated_points = geometry_util.resample_points_ring(points, n_samples)
+    pred_angles, interpolated_points = _resample_points_ring(points, n_samples)
     np.testing.assert_allclose(actual_angles, pred_angles)
     point_metric = geometry_util.integrated_point_metric(interpolated_points)
-    angular_metric = geometry_util.integrated_angle_metric(actual_angles)
+    angular_metric = _integrated_angle_metric(actual_angles)
     np.testing.assert_allclose(point_metric, angular_metric, rtol=1e-3)
-
-
-def test_walk_manifold():
-    n_dims = 5
-    n_samples = 10
-    start = np.random.uniform(-1, 1, n_dims)
-    end = np.random.uniform(-1, 1, n_dims)
-    start_phases = torch.from_numpy(start)
-    end_phases = torch.from_numpy(end)
-    walked = geometry_util.walk_manifold(start_phases, end_phases, n_samples).detach().numpy()
-    np.testing.assert_allclose(walked[0, :], start_phases)
-    np.testing.assert_allclose(walked[-1, :], end_phases)
-
-
-def test_circularization():
-    with torch.no_grad():
-        true_shift = np.random.uniform(-1, 1, 2)
-        true_rad = np.random.uniform(1, 3, 1)
-        angles = np.arange(start=0, stop=2 * np.pi, step=0.01)
-        x_vals = true_rad * np.cos(angles)
-        y_vals = true_rad * np.sin(angles)
-        points = np.stack([x_vals, y_vals], axis=-1) + true_shift
-
-        circularizer = geometry_util.CircleDistance(torch.from_numpy(true_shift), torch.from_numpy(true_rad))
-        circed = circularizer.circularize(torch.from_numpy(points)).numpy()
-        rad = np.sum(np.square(circed), axis=1)
-        np.testing.assert_allclose(rad, np.ones_like(rad))
-
-
-def test_center_finding():
-    true_shift = np.random.uniform(-1, 1, 2)
-    true_rad = np.random.uniform(1, 3, 1)
-    angles = np.arange(start=0, stop=2 * np.pi, step=0.01)
-    x_vals = true_rad * np.cos(angles)
-    y_vals = true_rad * np.sin(angles)
-    points = torch.from_numpy(np.stack([x_vals, y_vals], axis=-1) + true_shift)
-    n_iter = 200
-    circularizer = geometry_util.CircleDistance()
-    opt = torch.optim.Adam(circularizer.parameters(), lr=0.05)
-    for _ in range(n_iter):
-        opt.zero_grad()
-        distances = circularizer(points)
-        loss = torch.sum(distances)
-        loss.backward()
-        opt.step()
-    np.testing.assert_allclose(true_shift, circularizer.shift.detach().numpy(), rtol=1e-3)
-    np.testing.assert_allclose(true_rad, circularizer.rad.detach().numpy(), rtol=1e-3)
 
 
 def test_minimum_periodic_dist():
