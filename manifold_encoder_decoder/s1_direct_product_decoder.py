7df5a57c44b08b462e91718cb0eaf6eb033f7485
Trevor McCourt <trevormccourt@dhcp-10-29-113-179.dyn.mit.edu>
2023-01-13 15:34:27 -0500

    make decoder more modular

--- manifold_encoder_decoder/s1_direct_product_decoder.py
+++ manifold_encoder_decoder/s1_direct_product_decoder.py
@@ -5,6 +5,36 @@ import torch
 from manifold_encoder_decoder import geometry_util, encoder_decoder_core
 
 
+def decode_encode_cost(decoder, encoder, data):
+    decoded_points, decoded_angles = decoder(data)
+    re_encoded_points = encoder(decoded_points)
+    decoder_loss = torch.mean(torch.square(re_encoded_points - data))
+    return decoded_points, decoded_angles, re_encoded_points, decoder_loss
+
+
+def isometry_cost(encoder, decoded_angles, integration_resamples):
+    rolled_decoded_angles = torch.roll(decoded_angles, 1, dims=-2)
+    angular_distances, model_distances = encoder.minimum_straight_line_distance(decoded_angles,
+                                                                                    rolled_decoded_angles,
+                                                                                    n_points_integrate=integration_resamples)
+    normed_angular_distance = angular_distances / torch.mean(angular_distances)
+    normed_model_distance = model_distances / torch.mean(model_distances)
+    distance_cost = torch.mean(torch.square(normed_angular_distance - normed_model_distance))
+    return distance_cost
+
+
+def order_cost(encoder, re_encoded_points, decoded_angles, integration_resamples):
+    nearest_angular_distances, nearest_start, nearest_end, nearest_matches = geometry_util.closest_points_periodic(
+        decoded_angles)
+    nearest_re_encoded = torch.gather(re_encoded_points, -2,
+                                      torch.tile(torch.unsqueeze(nearest_matches, -1), [re_encoded_points.size(-1)]))
+    euclid_dist = torch.sqrt(torch.sum(torch.square(nearest_re_encoded - re_encoded_points), dim=-1) + 1e-13)
+    model_arclengths = \
+    encoder.minimum_straight_line_distance(nearest_start, nearest_end, n_points_integrate=integration_resamples)[1]
+
+    return torch.mean((model_arclengths - euclid_dist) / euclid_dist)
+
+
 def train(data, manifold_dim, device, encoder_hidden_dim=1500, encoder_n_hidden=1, decoder_hidden_dim=1500,
           decoder_n_hidden=1, integration_resamples=20, n_points_compare=20,
           batch_size=50, n_training_iterations=3000, loss_stop_thresh=1e-4, decoder_weight=1, order_red_weight=1):
@@ -24,24 +54,9 @@ def train(data, manifold_dim, device, encoder_hidden_dim=1500, encoder_n_hidden=
         samples = np.array([data[np.random.choice(sample_range, size=n_points_compare, replace=False), :] for i in range(batch_size)])
         data_samples = torch.tensor(samples, dtype=torch.get_default_dtype()).to(device)
         opt.zero_grad()
-        decoded_points, decoded_angles = decoder_net(data_samples)
-        re_encoded_points = encoder_net(decoded_points)
-        decoder_loss = torch.mean(torch.square(re_encoded_points - data_samples))
-
-        rolled_decoded_angles = torch.roll(decoded_angles, 1, dims=-2)
-        angular_distances, model_distances = encoder_net.minimum_straight_line_distance(decoded_angles, rolled_decoded_angles,
-                                                                                        n_points_integrate=integration_resamples)
-        normed_angular_distance = angular_distances / torch.mean(angular_distances)
-        normed_model_distance = model_distances / torch.mean(model_distances)
-        distance_cost = torch.mean(torch.square(normed_angular_distance - normed_model_distance))
-
-        nearest_angular_distances, nearest_start, nearest_end, nearest_matches = geometry_util.closest_points_periodic(decoded_angles)
-        nearest_re_encoded = torch.gather(re_encoded_points, -2, torch.tile(torch.unsqueeze(nearest_matches, -1), [re_encoded_points.size(-1)]))
-        euclid_dist = torch.sqrt(torch.sum(torch.square(nearest_re_encoded - re_encoded_points), dim=-1) + 1e-13)
-        model_arclengths = encoder_net.minimum_straight_line_distance(nearest_start, nearest_end, n_points_integrate=integration_resamples)[1]
-
-        norm_loss = torch.mean((model_arclengths - euclid_dist)/euclid_dist)
-
+        decoded_points, decoded_angles, re_encoded_points, decoder_loss = decode_encode_cost(decoder_net, encoder_net, data_samples)
+        distance_cost = isometry_cost(encoder_net, decoded_angles, integration_resamples)
+        norm_loss = order_cost(encoder_net, re_encoded_points, decoded_angles, integration_resamples)
         loss = (decoder_weight * decoder_loss) + distance_cost + (norm_loss * order_red_weight)
         loss.backward()
         opt.step()
