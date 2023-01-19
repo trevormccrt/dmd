833d41217338615fdcfc14d4569d5cba22bdc472
trevormccrt <trevormccrt@gmail.com>
2023-01-19 14:21:51 -0500

    all tests passing

--- encoder_decoder_core_test.py
+++ encoder_decoder_core_test.py
@@ -8,14 +8,12 @@ def test_periodic_euclid_vs_arclength():
     manifold_dim = 6
     embeded_dim = 90
     batch_size = 100
-    net = encoder_decoder_core.Encoder1D(embeded_dim, manifold_dim)
+    net = encoder_decoder_core.Encoder1D(embeded_dim, manifold_dim, 0)
     start_phases = torch.tensor(np.random.uniform(0, 2 * np.pi, (batch_size, manifold_dim)), dtype=torch.get_default_dtype())
     end_phases = torch.tensor(np.random.uniform(0, 2 * np.pi, (batch_size, manifold_dim)), dtype=torch.get_default_dtype())
     with torch.no_grad():
-        start_points = geometry_util.torch_angles_to_ring(start_phases)
-        end_points = geometry_util.torch_angles_to_ring(end_phases)
-        start_embedded = net(start_points)
-        end_embedded = net(end_points)
+        start_embedded = net(start_phases)
+        end_embedded = net(end_phases)
         euclid_dist = torch.sqrt(torch.sum(torch.square(end_embedded - start_embedded), dim=-1))
         arclength = net.minimum_straight_line_distance(start_phases, end_phases)[1]
     np.testing.assert_array_less(euclid_dist, arclength)
@@ -37,8 +35,3 @@ def test_encode_decode():
     with torch.no_grad():
         re_embed = encoder(phases)
     assert re_embed.size()[-1] == embedded_dimension
-    print("")
-
-
-test_encode_decode()
-
