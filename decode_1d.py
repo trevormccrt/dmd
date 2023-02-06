f8fd63a02f8071c55a4a8255d8962105bceb0cb5
trevormccrt <trevormccrt@gmail.com>
2023-02-06 13:33:58 -0500

    work

--- decode_1d.py
+++ decode_1d.py
@@ -12,12 +12,6 @@ def decode_encode_cost(decoder, encoder, data):
     return decoded_angles, re_encoded_points, decoder_loss
 
 
-def div_cost(decoded_angles, n_bins):
-    hist = torch.histc(decoded_angles, bins=n_bins, min=-np.pi, max=np.pi)/torch.numel(decoded_angles)
-    hist = hist/torch.sum(hist)
-    return torch.sum((hist - 1/n_bins)**2)#-torch.log(n_bins) - 1/n_bins * torch.sum(torch.log(hist))
-
-
 def distance_costs(encoder, re_encoded_points, decoded_angles, integration_resamples):
     nearest_angular_distances, nearest_end, nearest_matches = encoder.closest_points_on_manifold(
         decoded_angles)
@@ -47,7 +41,7 @@ def distance_costs(encoder, re_encoded_points, decoded_angles, integration_resam
 
 def train(data, n_circular_dimensions, n_linear_dimensions, device, encoder_hidden_dim=1500, encoder_n_hidden=1, decoder_hidden_dim=1500,
           decoder_n_hidden=1, integration_resamples=20, n_points_compare=20,
-          batch_size=50, n_training_iterations=3000, loss_stop_thresh=1e-4, decoder_weight=1, order_red_weight=1, div_weight=1, verbose=True, n_div_bins=10):
+          batch_size=50, n_training_iterations=3000, loss_stop_thresh=1e-4, decoder_weight=1, order_red_weight=1, div_weight=1, verbose=True):
 
     embedded_dim = np.shape(data)[1] # we will give the NN points on a ring in 2D as input
     encoder_net = encoder_decoder_core.Encoder1D(embedded_dim, n_circular_dimensions, n_linear_dimensions,
@@ -57,7 +51,6 @@ def train(data, n_circular_dimensions, n_linear_dimensions, device, encoder_hidd
 
     params = list(encoder_net.parameters()) + list(decoder_net.parameters())
     opt = torch.optim.Adam(params)
-    n_div_bins = torch.tensor(n_div_bins)
     sample_range = np.arange(start=0, stop=np.shape(data)[0], step=1)
     best_loss = np.inf
     best_encoder = copy.deepcopy(encoder_net)
@@ -69,11 +62,6 @@ def train(data, n_circular_dimensions, n_linear_dimensions, device, encoder_hidd
         opt.zero_grad()
         decoded_angles, re_encoded_points, decoder_loss = decode_encode_cost(decoder_net, encoder_net, data_samples)
         norm_loss, distance_cost, distribution_cost = distance_costs(encoder_net, re_encoded_points, decoded_angles, integration_resamples)
-        random_sample_phases = torch.tensor(np.random.uniform(-np.pi, np.pi, decoded_angles.shape), dtype=decoded_angles.dtype)
-        decoded_dists = torch.mean(encoder_net.closest_points_on_manifold(decoded_angles)[0])
-        sampled_dists = torch.mean(encoder_net.closest_points_on_manifold(random_sample_phases)[0])
-        #distribution_cost = torch.abs((sampled_dists - decoded_dists)/sampled_dists)
-        #distribution_cost = (torch.min(decoded_angles) - (-torch.pi))**2 + (torch.max(decoded_angles) - torch.pi)**2
         loss = (decoder_weight * decoder_loss) + distance_cost + (norm_loss * order_red_weight) + (div_weight * distribution_cost)
         loss.backward()
         opt.step()
